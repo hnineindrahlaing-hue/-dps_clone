1d928a96dc42693184bd10dffd4b3598db4e1dcc
hnineindrahlaing-hue <hnineindrahlaing@gmail.com>
2026-05-15 15:39:18 +0630

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,22 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 import "./.next/dev/types/routes.d.ts";
+export const metadata = {
+  verification: {
+    google: 'cMaII85inaE9pjxozU7rGU6fRM50f_sI_rXycdouDuU',
+  },
+}
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
